c077d2adb5c83256a223947cf3e1fe1b4490fed8
NSQA <nguyensyquanganh@gmail.com>
2020-05-24 14:38:41 +0700

    Update auto sliding images

--- scripts/album.slide.js
+++ scripts/album.slide.js
@@ -11,52 +11,115 @@ let currentIndex1 = 1;
 let currentIndex2 = 0;
 let currentIndex3 = -1;
 
+
+const timeout = 3500;
+const delay = timeout * 3;
+
 window.onload = button1.click();
 
+let timeout1;
+let timeout2;
+let timeout3;
+
+//Set image pos
+const imgPos = (...images) => {
+    let i =1;
+    for (let img of images) {
+        img.style.zIndex = i;
+        i++;
+    }
+}
 
 
+const Images = {
+    addClass (img, className) {
+        img.classList.add(className);
+    },
+    removeClass (img, className) {
+        img.classList.remove(className);
+    }
+}
+
+
+const infiniteLoop = setInterval(() => {
+    timeout1 = setTimeout(() => {
+
+        Images.removeClass(img1, 'movement');
+        imgPos(img3,img1,img2);
+        
+        Images.addClass(img2, 'movement');
+
+        timeout2 = setTimeout(() => {
+            
+            Images.removeClass(img2, 'movement');
+            imgPos(img1, img2, img3);
+            
+            Images.addClass(img3, 'movement');
+            timeout3 = setTimeout(() => {
+            
+                Images.removeClass(img3, 'movement');
+                imgPos(img2, img3, img1);
+            
+                Images.addClass(img1, 'movement');
+                
+            }, timeout);
+        }, timeout);
+    }, timeout);
+
+}, delay);
+
+
+document.addEventListener('click', () => {
+    if (infiniteLoop) {
+        clearInterval(infiniteLoop);
+        clearTimeout(timeout1);
+        clearTimeout(timeout2);
+        clearTimeout(timeout3);
+    }
+})
 
 button1.addEventListener('click', () => {
     currentIndex1 = 1;
-    img1.style.zIndex = currentIndex1.toString();
-    img2.style.zIndex = currentIndex2.toString();
-    img3.style.zIndex = currentIndex3.toString();   
-    
-    img1.style.animation = 'slidein 1s ease';
-    img3.style.animation = '';  
-    img2.style.animation = '';
+    img1.style.zIndex = currentIndex1;
+    img2.style.zIndex = currentIndex2;
+    img3.style.zIndex = currentIndex3;
 
-    currentIndex1=0;
-    currentIndex2=-1;
-    currentIndex3=-1;
+    Images.addClass(img1, 'movement');
+    Images.removeClass(img3, 'movement');
+    Images.removeClass(img2, 'movement');
+
+
+    currentIndex1 = 0;
+    currentIndex2 = -1;
+    currentIndex3 = -1;
 });
 
 button2.addEventListener('click', () => {
     currentIndex2 = 1;
-    img1.style.zIndex = currentIndex1.toString();
-    img2.style.zIndex = currentIndex2.toString();
-    img3.style.zIndex = currentIndex3.toString();
-    
-    img2.style.animation = 'slidein 1s ease';
-    img1.style.animation = '';  
-    img3.style.animation = '';
-    
-    currentIndex2=0;
-    currentIndex1=-1;
-    currentIndex3=-1;
+    img1.style.zIndex = currentIndex1;
+    img2.style.zIndex = currentIndex2;
+    img3.style.zIndex = currentIndex3;
+
+    Images.addClass(img2, 'movement');
+    Images.removeClass(img1, 'movement');
+    Images.removeClass(img3, 'movement');
+
+    currentIndex2 = 0;
+    currentIndex1 = -1;
+    currentIndex3 = -1;
 });
 
 button3.addEventListener('click', () => {
     currentIndex3 = 1;
-    img1.style.zIndex = currentIndex1.toString();
-    img2.style.zIndex = currentIndex2.toString();
-    img3.style.zIndex = currentIndex3.toString();
+    img1.style.zIndex = currentIndex1;
+    img2.style.zIndex = currentIndex2;
+    img3.style.zIndex = currentIndex3;
 
-    img3.style.animation = 'slidein 1s ease';
-    img2.style.animation = '';  
-    img1.style.animation = '';
+    Images.addClass(img3, 'movement');
+    Images.removeClass(img2, 'movement');
+    Images.removeClass(img1, 'movement');
 
-    currentIndex3=0;
-    currentIndex1=-1;
-    currentIndex2=-1;
+    currentIndex3 = 0;
+    currentIndex1 = -1;
+    currentIndex2 = -1;
 });
---
+++ scripts/scroll.animation.js
@@ -0,0 +1,9 @@
+const left = document.getElementById('gender-left');
+const right = document.getElementById('gender-right');
+const heart = document.getElementById('info-heart');
+
+document.addEventListener('scroll', addAnimationToInfo);
+
+const addAnimationToInfo = () => {
+
+}
