d9c242fb955852681c8d425fc50efa7226318c89
NSQA <nguyensyquanganh@gmail.com>
2020-05-24 15:15:50 +0700

    Add effects

--- scripts/scroll.animation.js
+++ scripts/scroll.animation.js
@@ -2,8 +2,11 @@ const left = document.getElementById('gender-left');
 const right = document.getElementById('gender-right');
 const heart = document.getElementById('info-heart');
 
-document.addEventListener('scroll', addAnimationToInfo);
+
 
 const addAnimationToInfo = () => {
-
+    left.classList.add('moveup__left');
+    right.classList.add('moveup__right');
 }
+
+document.addEventListener('scroll', addAnimationToInfo);
